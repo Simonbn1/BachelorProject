c2b09b7e120585efe26c4f3f0c05cbfd833b5e57
simon <sini022@student.kristiania.no>
2026-02-25 19:35:33 +0100

    Initial local project

---
+++ backend/src/main/java/no/timeforing/BachelorProject/BachelorProjectApplication.java
@@ -0,0 +1,13 @@
+package no.timeforing.BachelorProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachelorProjectApplication.class, args);
+	}
+
+}
